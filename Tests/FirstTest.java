b434424ac8063c808e00a2d6877edb160615058e
Nadya <n.perehodko@yandex.com>
2018-11-15 14:03:25 +0300

    Add files via upload

---
+++ Tests/FirstTest.java
@@ -0,0 +1,29 @@
+package ru.deliveryClub;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty( "webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.delivery-club.ru/");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("«Delivery Club» — круглосуточная доставка из любимых ресторанов города"));
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
