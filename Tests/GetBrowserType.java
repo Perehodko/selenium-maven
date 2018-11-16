fb4b7858b9f46693515358d1bdf11d1f3a71703b
Nadya <n.perehodko@yandex.com>
2018-11-16 13:06:46 +0300

    Add files via upload

---
+++ Tests/GetBrowserType.java
@@ -0,0 +1,30 @@
+package ru.deliveryClub;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class GetBrowserType {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty( "webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        Capabilities caps = ((RemoteWebDriver)driver).getCapabilities();
+        String browserName = caps.getBrowserName();
+        String browserVersion = caps.getVersion();
+        System.out.println(browserName+ "" +browserVersion);
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
