d8061b750c8441eabf38c3240745e4932ec29d40
Nadya <n.perehodko@yandex.com>
2018-11-16 14:16:33 +0300

    Add files via upload

---
+++ Tests/ScrollToBottomOfPage.java
@@ -0,0 +1,29 @@
+package ru.deliveryClub;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScrollToBottomOfPage {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty( "webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://wikipedia.org");
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("scrollBy(0, 2500)");
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
+
