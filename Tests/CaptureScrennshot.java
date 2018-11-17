d7e827dc5f4ac5907db118e7f33d8fc12ccb5c18
Nadya <n.perehodko@yandex.com>
2018-11-17 14:47:10 +0300

    Add files via upload

---
+++ Tests/CaptureScrennshot.java
@@ -0,0 +1,37 @@
+package ru.deliveryClub;
+import com.google.common.io.Files;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.*;
+import java.io.IOException;
+
+public class CaptureScrennshot {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://www.delivery-club.ru/");
+
+        File screenFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            Files.copy(screenFile, new File("C:\\Users\\Надя\\Desktop\\screen.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+        }
+    }
