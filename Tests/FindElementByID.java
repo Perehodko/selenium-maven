9dc0f317ad5d8cdf204b99020aa3842e18fb99c6
Nadya <n.perehodko@yandex.com>
2018-11-17 17:42:53 +0300

    Add files via upload

---
+++ Tests/FindElementByID.java
@@ -0,0 +1,34 @@
+package ru.deliveryClub;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementByID {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://www.google.com/");
+
+        try {
+            driver.findElement(By.id("gbwa")).click();
+        }
+        catch (Exception e) {
+            System.out.println("Элемент не найден");
+        }
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
