f463da5c1a6bf509ae5ce01692a222d25dca1133
Nadya <n.perehodko@yandex.com>
2018-11-17 17:17:21 +0300

    Add files via upload

---
+++ Tests/ClickLinkByXPathClass.java
@@ -0,0 +1,32 @@
+package ru.deliveryClub;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClickLinkByXPathClass {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty( "webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://www.google.com/?hl=en");
+
+        driver.findElement(By.xpath("//a[@class='gb_P']")).click();
+
+        String currentURL = driver.getCurrentUrl();
+        System.out.println(currentURL);
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
