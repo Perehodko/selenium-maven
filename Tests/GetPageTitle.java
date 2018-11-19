76854e80649caa6630b65eb835a8fe150e2f35f1
Nadya <n.perehodko@yandex.com>
2018-11-19 19:03:52 +0300

    Add files via upload

---
+++ Tests/GetPageTitle.java
@@ -0,0 +1,30 @@
+package ru.deliveryClub;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetPageTitle {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://www.google.com/");
+
+        String title = driver.getTitle();
+        System.out.println(title);
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
