15a1022d05d125921827f85d5610577b8bb237cb
Nadya <n.perehodko@yandex.com>
2018-11-21 21:50:17 +0300

    Add files via upload

---
+++ Tests/ClickAButtonById.java
@@ -0,0 +1,36 @@
+package ru.deliveryClub;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClickAButtonById {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://www.yahoo.com/");
+
+        try {
+            Thread.sleep(5000);
+            driver.findElement(By.id("uh-signin"));
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
