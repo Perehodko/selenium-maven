685dbbb7dc4ee5c6a8f7b35fd89df5ff36d7af49
Nadya <n.perehodko@yandex.com>
2018-12-03 21:57:49 +0300

    Add files via upload

---
+++ Tests/SpecialCharactersKeyEvents.java
@@ -0,0 +1,36 @@
+package ru.deliveryClub;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SpecialCharactersKeyEvents {
+
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://www.wikipedia.org/");
+
+        WebElement element = driver.findElement(By.cssSelector("body"));
+
+        element.sendKeys(Keys.PAGE_DOWN);
+        element.sendKeys(Keys.F5);
+        element.sendKeys(Keys.PAGE_UP);
+
+    }
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
