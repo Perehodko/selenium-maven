be596419a1ab7178ab88c935d5fcc199e44dc55a
Nadya <n.perehodko@yandex.com>
2018-12-04 22:00:45 +0300

    Add files via upload

---
+++ Tests/VerifyElementIsEnabled.java
@@ -0,0 +1,37 @@
+package ru.deliveryClub;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class VerifyElementIsEnabled {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test() {
+        driver.get("https://www.google.com/");
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[3]/center/input[1]"));
+
+        if (!element.isEnabled())
+        {
+            System.out.println("Элемент не отображен");
+        }
+        else {
+            System.out.println("Элемент отображен");
+        }
+    }
+
+    @After
+    public void close() {
+        driver.close();
+    }
+}
