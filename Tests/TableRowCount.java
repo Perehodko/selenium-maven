079e00ca19f440a7f8fcdea08ab723695dcdc40f
Nadya <n.perehodko@yandex.com>
2018-12-13 22:48:56 +0300

    Update TableRowCount.java

--- Tests/TableRowCount.java
+++ Tests/TableRowCount.java
@@ -13,7 +13,7 @@ public class TableRowCount {
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nadya\\Downloads\\chromedriver.exe");
         driver = new ChromeDriver();
     }
 
