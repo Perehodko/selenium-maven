057e77bec8cc5131d2797011672dbb7d39baa23e
Nadya <n.perehodko@yandex.com>
2018-12-13 22:48:17 +0300

    Add files via upload

---
+++ Tests/TableRowCount.java
@@ -0,0 +1,32 @@
+package ru.deliveryClub;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TableRowCount {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test() {
+        driver.get("https://en.wikipedia.org/wiki/Leopard_seal");
+
+        List<WebElement> txt = (List<WebElement>) driver.findElements(By.cssSelector("table.infobox.biota>tbody>tr"));
+        System.out.println("Количество строк в таблице: " + txt.size());
+    }
+
+    @After
+    public void close() {
+        driver.close();
+    }
+}
