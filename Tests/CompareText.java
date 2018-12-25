eb9ca7181f53d6ad8d2447b723309ecfa2a91239
Nadya <n.perehodko@yandex.com>
2018-12-25 19:32:09 +0300

    Update and rename FirstTest.java to CompareText.java

--- Tests/CompareText.java
+++ Tests/CompareText.java
@@ -10,8 +10,8 @@ public class FirstTest {
     public ChromeDriver driver;
 
     @Before
-    public void setUp(){
-        System.setProperty( "webdriver.chrome.driver", "C:\\Users\\Надя\\Downloads\\chromedriver.exe");
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nadya\\Downloads\\chromedriver.exe");
         driver = new ChromeDriver();
     }
 
